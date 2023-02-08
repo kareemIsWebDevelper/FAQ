31b56d60094c1fb3fc393ffe96692e59b98c9594
kareemIsWebDeveloper <kareem.fullstackdeveloper@gmial.com>
2023-02-08 20:05:04 +0200

    fAQ components

---
+++ main.js
@@ -0,0 +1,10 @@
+// const app = Vue.createApp({
+//     data: function (){
+//         return {
+//             description: 'loading image',
+//             img: './images/illustration-woman-online-mobile.svg',
+//             pattern: './images/bg-pattern-mobile.svg',
+//             shape: './images/bg-pattern-desktop.svg'
+//         }
+//     }
+// })
